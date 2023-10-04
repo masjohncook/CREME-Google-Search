25f33e9b7fb547a6e7e6df25113a3752bbdc6750
masjohncook <mas.john.cook@gmail.com>
2023-10-04 09:54:25 +0800

    fix - add timeout to 2 hours

--- src/google_search.py
+++ src/google_search.py
@@ -68,7 +68,7 @@ class GSearch():
                 if "429 Client Error: Too Many Requests" in error_message:
                     print("Too many request, sleep for 10 minutes")
                     self.logger.info("Too many request, sleep for 90 minutes")
-                    time.sleep(4500)
+                    time.sleep(7200)
                 else:
                     self.logger.info(f"Error occurred while searching: {e}")
                     break
